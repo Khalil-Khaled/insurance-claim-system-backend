1102a59c43cd91ba192c2c819519651561c956bd
Khalil Khaled <khalilkhaled1995@gmail.com>
2026-04-12 15:50:50 +0100

    initial commit

---
+++ src/main/java/com/insurance/claim_system/ClaimSystemApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.claim_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimSystemApplication.class, args);
+	}
+
+}
