35d723b22b08378af97d8f136280aeea8fd21db7
Khalil Khaled <khalilkhaled1995@gmail.com>
2026-04-12 16:03:47 +0100

    fix missing database config issue

--- src/main/java/com/insurance/claim_system/ClaimSystemApplication.java
+++ src/main/java/com/insurance/claim_system/ClaimSystemApplication.java
@@ -2,8 +2,9 @@ package com.insurance.claim_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ClaimSystemApplication {
 
 	public static void main(String[] args) {
